c678f137650f0813ae07e43c8ae41bba630d2c21
GintarasKapocius <gintaraskapocius@gmail.com>
2021-11-15 18:56:52 +0200

    4: API.js

---
+++ JS/API.js
@@ -0,0 +1,21 @@
+const baseUrl = 'http://localhost:3000'
+
+class API {
+  static fetchApartments = (success, failure) => {
+    fetch(`${baseUrl}/apartments`)
+      .then((res) => res.json())
+      .then(success)
+      .catch(failure)
+  }
+
+  static deleteApartment = (id, success, failure) => {
+    fetch(`${baseUrl}/apartments/${id}`, { method: 'DELETE' })
+      .then((res) => (res.ok ? success() : failure(res.statusText)))
+      .catch(failure)
+  }
+}
+
+//Check Data
+
+// API.fetchApartments(console.log, console.error)
+// API.deleteApartment('2', () => console.log('Item deleted'), console.error)
